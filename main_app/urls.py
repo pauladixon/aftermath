1c5c7e4c2b66a1b459f2f995da0cdb5be340c325
koashima <48111257+koashima@users.noreply.github.com>
2020-05-03 14:23:35 -0700

    Adds functionality to update and delete posts

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('posts/', views.posts_index, name='posts_index'),
     path('posts/<int:post_id>/', views.posts_detail, name='posts_detail'),
     path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
+    path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
+    path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
