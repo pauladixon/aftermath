40b3f91fe1e616eeba1e5bec480c581da1b15020
Bart Webber <bartacus.webber@gmail.com>
2020-05-03 12:12:09 -0700

    added detail views for posts and added create post funcionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Challenge, Post
-# Create your views here.
+
 
 def home(request):
     return render(request, 'home.html')
@@ -13,21 +14,30 @@ def home(request):
 def about(request):
     return render(request, 'about.html')    
 
+@login_required
 def challenges_index(request):
     challenges = Challenge.objects.all()
     return render(request, 'challenges/index.html', {'challenges': challenges})
 
+@login_required
 def posts_index(request):
     posts = Post.objects.all()
     return render(request, 'posts/index.html', {'posts': posts})
 
-class PostCreate(CreateView):
+@login_required
+def posts_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'posts/detail.html', { 'post': post})
+
+
+class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['name']
 
     def form_valid(self, form):
       form.instance.user = self.request.user  
       return super().form_valid(form)
+
 
 def signup(request):
     error_message = ''
@@ -41,4 +51,7 @@ def signup(request):
             error_message = 'Invalid sign up - try again' 
     form = UserCreationForm()
     context = { 'form': form, 'error_message': error_message} 
-    return render(request, 'registration/signup.html', context)          
+    return render(request, 'registration/signup.html', context)      
+
+
+           
