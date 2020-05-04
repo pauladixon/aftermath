38220f1734d47a2dcc026faa03212e76f1f7a870
Terrell Lemons <terrelllemons@Terrells-iMac.home>
2020-05-04 09:19:03 -0700

    add blog

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-
+from .models import Category
 
 class Challenge(models.Model):
     name = models.CharField(max_length=100)
--- main_app/urls.py
+++ main_app/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
     path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
     path('accounts/signup/', views.signup, name='signup'),
+
+    path('blog/', views.add_comment, name='add_comment')
+    path('blog/', views.list_of_post, name='list_of_post')
+    path('blog/', views.post_detail, name='post_detail')
 ]
