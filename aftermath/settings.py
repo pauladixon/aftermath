57d632315c9a39a80a3e5cccf138361844e27a05
pauladixon <paulapdixon@gmail.com>
2020-07-14 18:45:50 -0700

    config new deployment

--- aftermath/settings.py
+++ aftermath/settings.py
@@ -125,11 +125,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 STATICFILES_DIRS = (
-    normpath(join(BASE_DIR, 'static')),
-    normpath(join(BASE_DIR, 'upload')),
+    os.path.join(os.path.normpath(BASE_DIR), "static")
 )
-
-STATIC_ROOT = normpath(join(BASE_DIR, 'assets'))
 
 import environ
 environ.Env()
